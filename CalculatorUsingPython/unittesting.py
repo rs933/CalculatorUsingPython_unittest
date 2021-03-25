3ccd9d2d341a8997aba3547a2ef653c16ac8030f
rs933 <rs933@njit.edu>
2021-03-25 03:10:46 -0400

    added testingDivision function and csv file to test this funtion automatically

--- CalculatorUsingPython/unittesting.py
+++ CalculatorUsingPython/unittesting.py
@@ -38,16 +38,16 @@ class TestMain(unittest.TestCase):
                 main_func = calculator.Calculator().multiplication(arg1, arg2)
                 self.assertEqual(main_func, result)
 
-    # def testingDivision(self):
-    #     with open('./Unit Test Division.csv', 'r') as addition:
-    #         data = csv.reader(addition)
-    #         converting_into_list = list(data)
-    #         for x in range(1, len(converting_into_list)):
-    #             arg1 = int(converting_into_list[x][x - x])
-    #             arg2 = int(converting_into_list[x][x-x+1])
-    #             result = int(converting_into_list[x][x-x + 2])
-    #             main_func = calculator.Calculator().division(arg1, arg2)
-    #             self.assertEqual(main_func, result)
+    def testingDivision(self):
+        with open('./Unit Test Division.csv', 'r') as addition:
+            data = csv.reader(addition)
+            converting_into_list = list(data)
+            for x in range(1, len(converting_into_list)):
+                arg1 = int(converting_into_list[x][x - x])
+                arg2 = int(converting_into_list[x][x-x+1])
+                result = int(converting_into_list[x][x-x + 2])
+                main_func = calculator.Calculator().division(arg1, arg2)
+                self.assertEqual(main_func, result)
 
     # def testingSquare(self):
     #     with open('./Unit Test Square.csv', 'r') as addition:
